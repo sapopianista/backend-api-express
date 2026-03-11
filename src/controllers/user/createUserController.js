195a827e5452f0f92b3dce45c8a4497823882320
Tiago Vieira <tiagoferreiravieira167@gmail.com>
2026-03-11 13:50:42 -0300

    fixing some bugs related to the prisma

--- src/controllers/user/createUserController.js
+++ src/controllers/user/createUserController.js
@@ -1,3 +1,18 @@
-export function createUserController(req, res){
-    res.send('Exemplo de POST na rota /user respondida pelo Cotroller!')
+import { createUser } from "../../models/userModel.js"
+
+export async function createUsersController(req, res){
+
+    const user = {
+        avatar: "https://github.com/renancavichi.png",
+        name: "Renan Cavichi",
+        email: "renancavichi@gmail.com",
+        pass: "12345678"
+    }
+
+    const result = await createUser(user)
+
+    res.json({
+        message: "Usuário criado com sucesso!",
+        user: result
+    })
 }
